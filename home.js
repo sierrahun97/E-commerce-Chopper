d6324cd04f892dd278739cebbff1f4f966bb9260
MariprogrammerUwU <programmerprojectmari@gmail.com>
2024-09-27 22:36:07 -0500

    Empezar vista home

---
+++ home.js
@@ -0,0 +1,27 @@
+
+const track = document.querySelector('.carousel-track');
+const prevButton = document.querySelector('.carousel-btn-prev');
+const nextButton = document.querySelector('.carousel-btn-next');
+let currentSlide = 0;
+
+function updateCarouselPosition() {
+  const slideWidth = document.querySelector('.carousel-slide').clientWidth;
+  track.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+  const totalSlides = document.querySelectorAll('.carousel-slide').length;
+  if (currentSlide < totalSlides - 1) {
+    currentSlide++;
+    updateCarouselPosition();
+  }
+});
+
+prevButton.addEventListener('click', () => {
+  if (currentSlide > 0) {
+    currentSlide--;
+    updateCarouselPosition();
+  }
+});
+
+window.addEventListener('resize', updateCarouselPosition);
