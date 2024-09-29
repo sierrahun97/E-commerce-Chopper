c58b7d11db0cb5b0c6a3ccb93b931cffb13a4ef5
sierrahun97 <jp200801@hotmail.com>
2024-09-29 10:24:12 -0500

    Método addItem

--- js/catalog.js
+++ js/catalog.js
@@ -1,14 +1,33 @@
-const ItemsControllers = require('./itemsController.js')
+const ItemsControllers = require('./itemsController.js');
+
 
 const items = new ItemsControllers();
-console.log(items)
-
-const name = "dogshow";
-const price = 15000;
-const description = "comida para perro";
-const url = "dogshow.com";
-const category = "Perro";
 
 
-items.addItem(name, price, description, url, category)
-console.log(items)
+items.addItem('hills1', 15000, 'hill.com', 'comida para perro', 'Perros');;
+items.addItem('hills2', 15000, 'hill2.com', 'comida para perro', 'Perros');
+items.addItem('hills3', 15000, 'hill3.com', 'comida para perro', 'Perros');
+items.addItem('hills4', 15000, 'hill4.com', 'comida para perro', 'Perros');
+items.addItem('hills5', 15000, 'hill1.com', 'comida para perro', 'Perros');
+
+console.log(items.items);
+
+const productsSection = document.querySelector(".catalog-products");
+document.addEventListener("DOMContentLoaded", function () {
+    productsSection.innerHTML = '';
+    items.items.forEach(item => {
+        productsSection.innerHTML += `
+            <div class="item-catalog">
+                <div class="img-product">
+                    <img src="${item.url}" alt="image-product">
+                </div>
+                <div class="info-product">
+                    <h5>${item.name}</h5>
+                    <p id="product-price">$${item.price}</p>
+                    <p>${item.description}</p>
+                    <p>${item.category}</p>
+                    <button class="add-to-cart">Añadir</button>
+                </div>
+            </div>`;
+    })
+})
--- js/itemsController.js
+++ js/itemsController.js
@@ -1,28 +1,34 @@
-const Product = require("./ProductClass.js")
 class ItemsController {
     constructor(currentId = 0) {
         this.items = [];
         this.currentId = currentId;
     }
 
-    addItem(name, price, description, url, category) {
-        this.currentId++;
-        // Se instancia el objeto de la clase producto
-        const newProduct = new Product(this.currentId, name, description, price, url, category)
-        this.items.push(newProduct);
-
-        // Creación manual del objeto
-        //     const product =   {
-        // //         id: this.currentId,
-        // //         name: name,
-        // //         price: price,
-        // //         description: description,
-        // //         url: url,
-        // //         category: category,
-        // //     }
-        // //     this.items.push(product);
+    addItem(name, price, url,description, category) {
+        const product =   {
+            id: this.currentId++,
+            name: name,
+            price: price,
+            description: description,
+            url: url,
+            category: category,
+        }
+        this.items.push(product);
     }
 }
 
 module.exports = ItemsController
 
+        // const product= {
+        // id :this.currentId++,
+        // name : name, 
+        // description : description, 
+        // price : price, 
+        // url , 
+        // category
+        // }
+        // // Se instancia el objeto de la clase producto
+
+        // const newProduct = new Product(this.currentId, name, description, price, url, category)
+        // this.items.push(newProduct);
+        // console.log(this.items)
