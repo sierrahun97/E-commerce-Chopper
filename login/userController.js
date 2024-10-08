9bce8683882f1719e3eaf3562a5619ec39adddc5
sierrahun97 <jp200801@hotmail.com>
2024-10-08 18:00:16 -0500

    error codificacion

--- login/userController.js
+++ login/userController.js
@@ -1,11 +1,12 @@
-class UsersController{
-    constructor (currentUserId = 0){
+class UsersController {
+    constructor(currentUserId = 0) {
         const storedUserId = localStorage.getItem('currentUserId');
-        this.currentUserId = storedUserId ? parseInt(storedUserId) : currentUserId; 
+        this.currentUserId = storedUserId ? parseInt(storedUserId) : currentUserId;
         this.users = JSON.parse(localStorage.getItem('registeredUsers')) || [];
 
     }
 
+    
     addUser(userName, userEmail, userPhone, userPassword) {
         const user = {
             id: this.currentUserId++,
@@ -15,7 +16,14 @@ class UsersController{
             userPassword: userPassword,
             userRole: "CUSTOMER_USER"
         };
-
+        async function hashPassword(userPassword) {
+            const encoder = new TextEncoder(); // Codificar la contraseña como bytes
+            const data = encoder.encode(userPassword); // Convertir a formato de bytes
+            const hashBuffer = await crypto.subtle.digest('SHA-256', data); // Crear el hash
+            const hashArray = Array.from(new Uint8Array(hashBuffer)); // Convertir el hash a una matriz de bytes
+            const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join(''); // Convertir a hexadecimal
+            return hashHex;
+        }
         if (user.userEmail == "admin@chopper.admin.com") user.userRole = "ADMIN";
         this.users.push(user);
 
@@ -23,7 +31,12 @@ class UsersController{
         localStorage.setItem('registeredUsers', JSON.stringify(this.users));
         return user;
     }
+
+
+
 }
+
+
 
 // Exportar una instancia de UsersController
 export const userController = new UsersController();
