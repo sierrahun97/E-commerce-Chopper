7b29c0dbdc4e517c12ceb5ddad634f1bf94a2b8d
MariprogrammerUwU <programmerprojectmari@gmail.com>
2024-10-18 21:13:20 -0500

    restructuracion proyecto

--- login/user.js
+++ login/user.js
@@ -26,20 +26,20 @@ btnRegister.addEventListener('click', function (event) {
     if (!userName || !userEmail || !userPhone || !userPassword) {
         alert('Por favor, completa todos los campos.');
         return;
-    }else {
+    } else {
         const users = getUsersFromLocalStorage();
 
 
-    // Verifica si el usuario ya existe
-    const userExists = users.find(u => u.userEmail === userEmail);
-    if (userExists) {
-        alert('El usuario ya está registrado.');
-        return;
-    }
+        // Verifica si el usuario ya existe
+        const userExists = users.find(u => u.userEmail === userEmail);
+        if (userExists) {
+            alert('El usuario ya está registrado.');
+            return;
+        }
 
         // Crea el nuevo usuario
         const newUser = userController.addUser(userName, userEmail, userPhone, userPassword)
-        
+
         // Agrega el nuevo usuario y guarda en localStorage
         users.push(newUser);
         saveUsersToLocalStorage(users);
@@ -50,20 +50,32 @@ btnRegister.addEventListener('click', function (event) {
 
 // Inicio de sesión
 btnLogin.addEventListener('click', function (event) {
+   
     event.preventDefault();
 
     const email = document.querySelector('#login-email').value;
     const password = document.querySelector('#login-password').value;
-
     const users = getUsersFromLocalStorage();
-    const user = users.find(u => u.userEmail === email && u.userPassword === password);
+    let decodedPassword = '';
+    const shift = 3; // Número que defines para cambiar la posición de cada carácter (puede ser cualquier valor)
+
+    for (let i = 0; i < password.length; i++) {
+        const charCode = password.charCodeAt(i); // Obtener el código ASCII del carácter
+        const newCharCode = charCode + shift; // Cambiar la posición sumando un valor al código ASCII
+        decodedPassword += String.fromCharCode(newCharCode); // Convertir el nuevo código ASCII de vuelta a carácter
+
+    }
+
+    const user = users.find(u => u.userEmail === email && u.userPassword === decodedPassword);
 
     if (user) {
         localStorage.setItem('loggedInUser', JSON.stringify(user));
-        window.location.href = '../pages/home.html';  
+        window.location.href = '../pages/home.html';
     } else {
         alert('Correo o contraseña incorrectos.');
+
     }
+
 });
 
 
