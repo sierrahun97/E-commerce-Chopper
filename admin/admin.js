545d559b0c172230473ef548045bc4846456161b
sanrvallejo <zanti-23@hotmail.com>
2024-11-07 11:03:02 -0500

    fetch: consumir api server

--- admin/admin.js
+++ admin/admin.js
@@ -32,9 +32,9 @@ function loadProducts() {
     
 }
 
-function sendProducts(){
+function sendProducts(producto){
     //enviar datos al local storage
-    localStorage.setItem("listaproductos", JSON.stringify(itemsController.items));
+    localStorage.setItem("listaproductos", JSON.stringify(producto));
     loadProducts();
     // Limpiar los campos
     document.getElementById('productForm').reset();
@@ -46,24 +46,56 @@ document.addEventListener("DOMContentLoaded", function() {
     loadProducts();
 });
 
-document.getElementById('productForm').addEventListener('submit', function(event) {
+document.getElementById('productForm').addEventListener('submit', async function(event) {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
     // Obtener los valores de los campos
+    const codeProduct = document.getElementById('codeProduct').value;
     const productName = document.getElementById('productName').value;
     const category = document.getElementById('category').value;
     const price = document.getElementById('price').value;
+    const stockP = document.getElementById('stock').value;
+    const discount = document.getElementById('vip-discount').value;
     const image = document.getElementById('image').value;
     const description = document.getElementById('description').value;
+
+    const producto = {
+        codigo_producto: codeProduct,
+        nombre_producto: productName,
+        categoria_producto: category,
+        precio: price,
+        stock: stockP,
+        descuento_vip: discount,
+        descripcion_producto: description,
+        url: image,
+    }
 
     // Validación simple (puedes mejorar esta lógica)
     if (!productName || !category || !price || !image || !description) {
         alert('Por favor, completa todos los campos.');
         return;
     }else{
-        alert('Producto agregado correctamente!');
-        itemsController.addItem(productName, price, image, description, category);
-        sendProducts();
+        try {
+            let responseGenerated = await fetch('http://localhost:8080/producto/crear', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(producto)
+            }).then(response => {
+                console.log(response);
+                if (response.ok) {
+                    alert('Producto agregado correctamente!');
+                    itemsController.addItem(productName, price, image, description, category);
+                    sendProducts(producto);
+                }else {
+                    alert('Error al crear el producto');
+                }
+            })
+        } catch (error) {
+            console.error("Error al realizar la operación:", error);
+        }
+
     }
 
     itemsController.currentId()
